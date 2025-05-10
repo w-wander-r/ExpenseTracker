8b8607ee8f2652683f26f9554312574a2a54e2d9
w-wander-r <wanderexe68@gmail.com>
2025-05-10 22:50:16 +0300

    implementing validation for user input: validation dependency added ,UserDTO changed

--- backend/ExpenseTracker/src/main/java/com/wander/ExpenseTracker/model/UserDTO.java
+++ backend/ExpenseTracker/src/main/java/com/wander/ExpenseTracker/model/UserDTO.java
@@ -2,10 +2,21 @@ package com.wander.ExpenseTracker.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class UserDTO {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 32, message = "Username must be between 3 and 32 characters")
     private String username;
+    
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, max = 32, message = "Password must be between 8 and 32 characters")
     private String password;
+    
+    @NotNull(message = "Role cannot be null")
     private Role role;
 
     public Role getRole() {
